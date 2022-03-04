5c7c7d86895bb7d7a21db9caadde521364124c9d
shivaprasadmakthala <mshivaprasadg15@gmail.com>
2022-03-05 00:58:19 +0530

    Task executed

---
+++ script.js
@@ -0,0 +1,10 @@
+
+console.log(typeof(1));
+console.log(typeof(1.1));
+console.log(typeof('1.1'));
+console.log(typeof(true));
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof([]));
+console.log(typeof({}));
+console.log( typeof(NaN));
